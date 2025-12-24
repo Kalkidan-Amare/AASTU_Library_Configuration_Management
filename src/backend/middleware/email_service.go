0b2f6fd2ea5ff10aff182d1246de8a32c98df2d7
kalkidan-amare <kalkidanamare11a@gmail.com>
2025-12-24 21:59:15 +0300

    feat(auth): add forgot password controller and email delivery

---
+++ src/backend/middleware/email_service.go
@@ -0,0 +1,29 @@
+package middleware
+
+import (
+	"crypto/tls"
+	"fmt"
+
+	gomail "gopkg.in/mail.v2"
+)
+
+func SendOTPEmail(email, otp string) error {
+	// Set up the email
+	m := gomail.NewMessage()
+	m.SetHeader("From", "kalkidanamare11a@gmail.com")
+	m.SetHeader("To", email)
+	m.SetHeader("Subject", "Your OTP Code")
+	m.SetBody("text/plain", fmt.Sprintf("Your OTP code is %s", otp))
+
+	// Set up the SMTP server
+	d := gomail.NewDialer("smtp.gmail.com", 587, "kalkidanamare11a@gmail.com", "jcwf vfzi njtd rayo")
+	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	// Send the email
+	if err := d.DialAndSend(m); err != nil {
+		return err
+	}
+
+	return nil
+}
+
